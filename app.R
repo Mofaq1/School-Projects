cb771b7bbe213cf9a4887da20ed70b6f283ac167
Mofaq1 <125330541+Mofaq1@users.noreply.github.com>
2023-02-14 10:16:41 -0600

    Add files via upload
    App for online Survey

--- app.R
+++ app.R
@@ -1,65 +1,133 @@
-# Project #1
-# Creating a Virtual Calculator
-
-
 library(shiny)
+library(DBI)
+library(RSQLite)
+library(ggplot2)
+library(graphics)
+library(DT)
 
+
+
+con <- dbConnect(SQLite(), dbname = "survey_data.db")
+survey = data.frame(Q1 = "", Q2 = "", Q3 = "", Q4 = "")[-1,]
+dbWriteTable(con, "Survey", survey, overwrite = FALSE, append = TRUE)
+
+
+
+# Defining UI for the application for doing an online survey
 ui <- fluidPage(
-  
   # Application title
-  titlePanel("A Simple Calculator"), 
+  titlePanel("Online Survey"),
+ 
+  sidebarLayout(
+    sidebarPanel(
+  # Radio button function for Question 1
+  radioButtons(inputId = "rb", label = "What is your biological sex?",
+               c("Male" = "M",
+                 "Female" = "F")),
   
-  # Sidebar with a numeric input and two slider inputs
-  sidebarPanel(
-    numericInput(inputId = "x",
-                 label = "Type a number:",
-                 value = 10,
-                 step = 1
-    ), 
-    
-    numericInput(inputId = "y",
-                 label = "Type another number:",
-                 value = 10,
-                 step = 1
-    ),
-    
-    actionButton(inputId = "multiply", 
-                 label = "Multiply"), 
-   
-    
-    # Adding another button with the label "Add"
-     actionButton(inputId = "add", 
-                 label = "Add")
-    
-  ), 
+  # NumericInput function for Question 2
+  numericInput(inputId = "obs", label = "What is your age in years?", "Age"),
+
   
+  # SelectInput function for Question 3
+  selectInput(inputId = "id", 
+              label = "How much time do you spend on shopping outside on average each week?",
+              choices = c("One hour or less", 
+                          "More than an hour up to Two hours", 
+                          "Two to Three hours", 
+                          "More than Three hours")),
  
   
-  # Show outputs
+  # textAreaInput function for Question 4
+  textAreaInput(inputId = "cap", 
+                label = "What is your opinion about the handling of COVID-19 by President Biden?", 
+                "Write a paragraph of no more than 100 words"),
+   
+  
+  actionButton(inputId = "save",
+               label = "Save"),
+    ),
+  
   mainPanel(
-    textOutput(outputId = "result"),  
+    DTOutput("datatable"),
+    plotOutput(outputId = "rbChart"),
+    plotOutput(outputId = "obsChart"),
+    plotOutput(outputId = "idChart")
+    
   )
   
-)
+))
 
-# Server logic
-server <- function(input, output) {  
+
+server <-function(input, output, session){
   
-  observeEvent(input$multiply, {
-    output$result <- renderText({  
-      input$x * input$y
-    })
-    
+  observeEvent(input$save, {
+    dbExecute(con, statement = "INSERT INTO Survey VALUES (?, ?, ?, ?)",
+    params = list(input$rb, input$obs, input$id, input$cap)
+  )
+  session$reload()
   })
   
-  observeEvent(input$add, {
-    output$result <- renderText({  
-      input$x + input$y
-    })
-    
+  D = reactive({
+    dbGetQuery(con, statement = 'SELECT * FROM Survey')
   })
+  
+  split.screen(c(2,1)) # Makes Screen 1 and 2
+  split.screen(c(1,2), screen=1) # Makes Screen 3 and 4
+
+
+  
+  output$rbChart <- renderPlot(
+    {
+      screen(2)
+      ggplot(D(), aes(x = Q1)) + geom_bar(color = "white", fill = "steelblue")})
+
+  
+  output$obsChart <- renderPlot(
+    {
+      screen(3)
+      ggplot(D(), aes(x = as.numeric(Q2))) + geom_histogram(bins = 30, color = "white", fill = "steelblue")})
+  
+  
+  output$idChart <- renderPlot(
+    {
+      screen(4)
+      ggplot(D(), aes(x = Q3)) + geom_bar(color = "white", fill = "steelblue")}) 
+  
+  ##  Close all screens.
+  close.screen( all = TRUE )
+  
+  output$datatable <- renderDT({
+    D()
+  })   
 }
 
-# Link ui and server
-shinyApp(ui, server)
+
+# Part 5: Run the application 
+
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
